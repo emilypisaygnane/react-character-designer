f215f3a8664269cb7c41a3e710f73f259236c717
Emily Pisaygnane <episaygnane@me.com>
2022-09-15 12:25:05 -0700

    added clothing images for character, and renamed variables

--- src/components/character/character.js
+++ src/components/character/character.js
@@ -1,11 +1,11 @@
-export default function Character({ head, body, bottoms }) {
+export default function Character({ tops, bottoms, shoes }) {
   return ( 
     <div className="character">
       <h1>title:</h1>
       <div className="images">
-        <img src={`${process.env.PUBLIC_URL}/ ${head}.png`} /> 
-        <img src={`${process.env.PUBLIC_URL}/ ${body}.png`} />
+        <img src={`${process.env.PUBLIC_URL}/ ${tops}.png`} /> 
         <img src={`${process.env.PUBLIC_URL}/ ${bottoms}.png`} />
+        <img src={`${process.env.PUBLIC_URL}/ ${shoes}.png`} />
       </div>
     </div>
   );
--- src/components/display/display.js
+++ src/components/display/display.js
@@ -1,9 +1,9 @@
-export default function Display({ headCount, bodyCount, bottomCount }) {
+export default function Display({ topCount, bottomCount, shoeCount }) {
   return (
     <div className="display">
-      <p>You have changed the head ${headCount} times, 
-         the body ${bodyCount} times,
-         and the pants ${bottomCount} times. 
+      <p>You have changed the head ${topCount} times, 
+         the body ${bottomCount} times,
+         and the pants ${shoeCount} times. 
          And nobody can forget your character's classic catchphrases:
       </p>
     </div>
